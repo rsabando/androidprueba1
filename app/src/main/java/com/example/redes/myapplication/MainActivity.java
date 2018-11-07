37b3ee3710b4a416bab56625fceba7a4ba9ae6f0
rsabando <rsabando@espol.edu.ec>
2018-11-07 07:30:54 -0500

    ciclo de actividades android

--- app/src/main/java/com/example/redes/myapplication/MainActivity.java
+++ app/src/main/java/com/example/redes/myapplication/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.redes.myapplication;
-
+import android.util.Log;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +9,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("Estoy en create");
+       //Log.d("hola");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setContentView(R.layout.activity_main);
+        System.out.println("Estoy en Resume");
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setContentView(R.layout.activity_main);
+        System.out.println("Estoy en Start");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        setContentView(R.layout.activity_main);
+        System.out.println("Estoy en Stop");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        setContentView(R.layout.activity_main);
+        System.out.println("Estoy en Destroy");
     }
 }
